4e46e1f6b02d2826d1c4d4637483a885477d3747
Vincent Leo Mattes <vincent.leo.mattes@hs-furtwangen.de>
2022-06-22 18:05:45 +0200

    fixed string formatting for port

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
 	//service.ConnectToMongoDB()
 
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	http.HandleFunc("/mongo/getAll", service.ConnectToMongoDB)
-	http.ListenAndServe(fmt.Sprintf(":$s", port), nil)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
